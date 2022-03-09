4b755c0e98703d28cc973a35ee7bb0e13b747935
pangkyl24 <77285931+pangkyl24@users.noreply.github.com>
2022-03-09 11:05:27 -0500

    Update Organizer.java

--- Organizer.java
+++ Organizer.java
@@ -2,7 +2,40 @@ import java.util.ArrayList;
 
 public class Organizer{
 
-	public int[] countPopularity(ArrayList<String[]> data){
+	private ArrayList<ArrayList<String>> data;
+	private ArrayList<ArrayList<String>> baseData;
+	private ArrayList<String[]> csvData;
+	
+	public Organizer(ArrayList<String[]> data){
+		
+		csvData = data; //Keep original CSV data with all information
+		
+		this.data = new ArrayList<ArrayList<String>>();
+		for(int i = 1; i < 70; i++){
+			this.data.add(new ArrayList<String>());
+			this.data.get(i-1).add(data.get(i)[2] + " " + data.get(i)[3]);
+			for(int j = 10; j < 15; j++){
+				this.data.get(i-1).add(data.get(i)[j]);
+			}
+		} //Create 2D ArrayList with the Data we want to work with - Person Name, Pref 1, Pref 2, Pref 3, Pref 4, Pref 5
+		
+		baseData = new ArrayList<ArrayList<String>>();
+		for(int i = 0; i < this.data.size(); i++){
+			baseData.add(this.data.get(i));
+		}
+		System.out.println(baseData);
+		//Create Copy of the ArrayList above to keep. One will be mutated, one will not.
+		
+	}
+	
+	public ArrayList<ArrayList<String>> getData(){
+		return data;
+	}
+	
+	
+	/*public int[] countPopularity(ArrayList<String[]> data){
+		//Organizes an Array counting the amount of people who want to do each event
+		//Index Number = Event Number
 		
 		int[] popularityCount = new int[19];
 		
@@ -12,12 +45,22 @@ public class Organizer{
 			}
 		}
 		return popularityCount;
-		//Organizes an Array counting the amount of people who want to do each event
-		//Index Number = Event Number
+		
+		
+	}*/
+	public int[] countPopularity(){
+		int[] popularityCount = new int[19];
+		for(int i = 0; i < data.size(); i++){
+			for(int j = 1; j < 6; j++){
+				popularityCount[Integer.parseInt(data.get(i).get(j))] ++;
+			}
+			
+		}
+		return popularityCount;
 		
 	}
 	
-	public ArrayList<ArrayList<String>> organizeData(ArrayList<String[]> data){
+	/*public ArrayList<ArrayList<String>> organizeData(ArrayList<String[]> data){
 		
 		ArrayList<ArrayList<String>> orgData = new ArrayList<ArrayList<String>>();
 		for(int i = 1; i < 70; i++){
@@ -28,8 +71,19 @@ public class Organizer{
 			}
 		}
 		return orgData;
-		//Modify Original Data to this Format of a 2D ArrayList
-		// Name, Choice 1, Choice 2, Choice 3, Choice 4, Choice 5
+		
+	}*/
+	
+	public int searchTopNum(int[] list){
+		
+		int largest = 1;
+		for(int i = 2; i < list.length; i++){
+			if (list[largest] < list[i]){
+				largest = i;
+			}
+		}
+		return largest;
+		
 		
 	}
 
