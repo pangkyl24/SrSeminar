9bb29daa2fbae94eb6bcfa6e412f707077b4b5af
pangkyl24 <77285931+pangkyl24@users.noreply.github.com>
2022-03-09 11:06:36 -0500

    Create Speaker.java

---
+++ Speaker.java
@@ -0,0 +1,11 @@
+public class Speaker{
+	
+	private String name;
+	private int presCount; //Number of times the presenter has presented
+
+	public Speaker(String name){
+		this.name = name;
+		presCount = 0;
+	}
+
+}
