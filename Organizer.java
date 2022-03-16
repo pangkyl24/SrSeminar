1a8513a53d3b42f4a6561bfa996b12f19074d78e
pangkyl24 <77285931+pangkyl24@users.noreply.github.com>
2022-03-16 15:15:25 -0400

    Update Organizer.java

--- Organizer.java
+++ Organizer.java
@@ -1,102 +1,16 @@
-/*import java.util.ArrayList;
-
-public class Organizer{
-
-	private ArrayList<ArrayList<String>> data;
-	private ArrayList<ArrayList<String>> baseData;
-	private ArrayList<String[]> csvData;
-	
-	public Organizer(ArrayList<String[]> data){
-		
-		csvData = data; //Keep original CSV data with all information
-		
-		this.data = new ArrayList<ArrayList<String>>();
-		for(int i = 1; i < 70; i++){
-			this.data.add(new ArrayList<String>());
-			this.data.get(i-1).add(data.get(i)[2] + " " + data.get(i)[3]);
-			for(int j = 10; j < 15; j++){
-				this.data.get(i-1).add(data.get(i)[j]);
-			}
-		} //Create 2D ArrayList with the Data we want to work with - Person Name, Pref 1, Pref 2, Pref 3, Pref 4, Pref 5
-		
-		baseData = new ArrayList<ArrayList<String>>();
-		for(int i = 0; i < this.data.size(); i++){
-			baseData.add(this.data.get(i));
-		}
-		System.out.println(baseData);
-		//Create Copy of the ArrayList above to keep. One will be mutated, one will not.
-		
-	}
-	
-	public ArrayList<ArrayList<String>> getData(){
-		return data;
-	}
-	
-	
-	/*public int[] countPopularity(ArrayList<String[]> data){
-		//Organizes an Array counting the amount of people who want to do each event
-		//Index Number = Event Number
-		
-		int[] popularityCount = new int[19];
-		
-		for(int i = 1; i < 70; i++){
-			for(int j = 10; j < 15; j++){
-				popularityCount[Integer.parseInt(data.get(i)[j])] ++;	
-			}
-		}
-		return popularityCount;
-		
-		
-	}*/
-	/*public int[] countPopularity(){
-		int[] popularityCount = new int[19];
-		for(int i = 0; i < data.size(); i++){
-			for(int j = 1; j < 6; j++){
-				popularityCount[Integer.parseInt(data.get(i).get(j))] ++;
-			}
-			
-		}
-		return popularityCount;
-		
-	}*/
-	
-	/*public ArrayList<ArrayList<String>> organizeData(ArrayList<String[]> data){
-		
-		ArrayList<ArrayList<String>> orgData = new ArrayList<ArrayList<String>>();
-		for(int i = 1; i < 70; i++){
-			orgData.add(new ArrayList<String>());
-			orgData.get(i-1).add(data.get(i)[2] + " " + data.get(i)[3]);
-			for(int j = 10; j < 15; j++){
-				orgData.get(i-1).add(data.get(i)[j]);
-			}
-		}
-		return orgData;
-		
-	}*/
-	
-	/*public int searchTopNum(int[] list){
-		
-		int largest = 1;
-		for(int i = 2; i < list.length; i++){
-			if (list[largest] < list[i]){
-				largest = i;
-			}
-		}
-		return largest;
-		
-		
-	}*/
-
-}*/
-
 import java.util.ArrayList;
 
 public class Organizer{
 	
 	ArrayList<Person> peopleList = new ArrayList<Person>();
+	int[] prio = new int[19];
+	int[] firstPrio = new int[19];
+	ArrayList<ArrayList<Integer>> semTable = new ArrayList<ArrayList<Integer>>(); // Timeslot x Seminar #
+	ArrayList<Speaker> speakerList = new ArrayList<Speaker>();
 	
 	public Organizer(ArrayList<String[]> data){
 		
+		//Get People  +  Preferences
 		for(int i = 1; i < 70; i++){
 			
 			peopleList.add(new Person(data.get(i)[2] + " " + data.get(i)[3], 
@@ -108,9 +22,32 @@ public class Organizer{
 			
 		}
 		
-		System.out.println(peopleList);
+		//System.out.println(peopleList);
+		
+		//Get Speakers  +  Seminars
+		/* for(int i = 1; i < 19; i++){
+			
+			
+		} */ //Work in Progress
 		
 	}
 	
+	public void setPrio(){
+		
+		prio = new int[19];
+		firstPrio = new int[19];
+		
+		for( Person person : peopleList ){
+			
+			for(int i = 0; i < person.getPrefs().size(); i++){
+				prio[person.getPrefs().get(i)] ++;
+			}
+			firstPrio[person.getPrefs().get(0)] ++;
+			
+		}
+		
+	}
+	
+	
+	
 }
-
