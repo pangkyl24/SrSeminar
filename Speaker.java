ddb72905b82bffb6f7a6a682f230dea78f2759f3
pangkyl24 <77285931+pangkyl24@users.noreply.github.com>
2022-03-21 17:11:40 -0400

    Update Speaker.java

--- Speaker.java
+++ Speaker.java
@@ -2,14 +2,17 @@ import java.util.ArrayList;
 
 public class Speaker{
 	
+	//var setup
 	private String name;
 	private ArrayList<String> semsName = new ArrayList<String>();
 	private ArrayList<Integer> semsNum = new ArrayList<Integer>();
-
+	
+	//CONTRUCTOR
 	public Speaker(String name){
 		this.name = name;
 	}
 	
+	//Adding a seminar
 	public void addSeminar(String semName, int semNum){
 		
 		this.semsName.add(semName);
@@ -17,6 +20,7 @@ public class Speaker{
 		
 	}
 	
+	//Returning vars
 	public ArrayList<String> getSemsName(){
 		return semsName;
 	}
